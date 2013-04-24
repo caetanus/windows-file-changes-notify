5b3405399f31919b735240765023c42326d0db12
Marcelo A Caetano <marcelo.caetano@titansgroup.com>
2013-04-24 20:15:23 -0300

    setup.py added.

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*-  coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+        name='winwatcher',
+        version='0.1.0',
+        author='Marcelo A. Caetano',
+        author_email='marcelo.caetano@titansgroup.com.br',
+        packages=find_packages(),
+        url='http://github.com/caetanus/windows-file-changes-notify'
+        )            
+
